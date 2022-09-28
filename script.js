b6f1264a6435e168ec3547787b035ab11da21908
sunnil24 <sunilsri.srivastava3@gmail.com>
2022-09-28 18:47:12 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Hello!');
+const updateButton = document.getElementById('updateDetails');
+const favDialog = document.getElementById('favDialog');
+const outputBox = document.querySelector('output');
+const selectEl = favDialog.querySelector('select');
+const confirmBtn = favDialog.querySelector('#confirmBtn');
+
+if (typeof favDialog.showModal !== 'function') {
+  favDialog.hidden = true;
+  /* a fallback script to allow this dialog/form to function
+     for legacy browsers that do not support <dialog>
+     could be provided here.
+  */
+}
+updateButton.addEventListener('click', () => {
+  if (typeof favDialog.showModal === 'function') {
+    favDialog.showModal();
+  } else {
+    outputBox.value =
+      'Sorry, the <dialog> API is not supported by this browser.';
+  }
+});
+
+selectEl.addEventListener('change', (e) => {
+  confirmBtn.value = selectEl.value;
+});
+
+favDialog.addEventListener('close', () => {
+  outputBox.value = `${
+    favDialog.returnValue
+  } button clicked - ${new Date().toString()}`;
+});
